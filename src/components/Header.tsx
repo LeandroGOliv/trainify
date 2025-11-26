79999277be71d7f1925a91693c3e51c7f708e601
Leandro <leandro.golub123@gmail.com>
2025-11-26 12:45:03 -0300

    feat: adiciona dark mode do shadcn

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,8 @@
+import { ModeToggle } from './ModeToggle'
 export default function Header() {
-  return <></>
+  return (
+    <>
+      <ModeToggle />
+    </>
+  )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { Toaster } from './components/ui/sonner.tsx'
 import * as TanStackQueryProvider from './integrations/tanstack-query/root-provider.tsx'
+import { ThemeProvider } from './components/ui/ThemeProvider.tsx'
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
@@ -38,12 +39,14 @@ if (rootElement && !rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
-      <ZodProvider>
-        <TanStackQueryProvider.Provider {...TanStackQueryProviderContext}>
-          <RouterProvider router={router} />
-          <Toaster />
-        </TanStackQueryProvider.Provider>
-      </ZodProvider>
+      <ThemeProvider>
+        <ZodProvider>
+          <TanStackQueryProvider.Provider {...TanStackQueryProviderContext}>
+            <RouterProvider router={router} />
+            <Toaster />
+          </TanStackQueryProvider.Provider>
+        </ZodProvider>
+      </ThemeProvider>
     </StrictMode>,
   )
 }
